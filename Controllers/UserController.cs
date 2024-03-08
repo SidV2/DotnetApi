3e1a82c7cd2a880a87dbaefa476994efafea0110
Sidhartha.Verma <sidhartha.verma@ibm.com>
2024-03-08 10:57:40 -0400

    Init Setup

---
+++ Controllers/UserController.cs
@@ -0,0 +1,62 @@
+using DotnetAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+    DataContextDapper _dapper;
+    public UserController(IConfiguration config)
+    {
+        _dapper = new DataContextDapper(config);
+    }
+
+    [HttpGet("TestConnection")]
+    public DateTime TestConnection()
+    {
+        return _dapper.LoadDataSingle<DateTime>("SELECT GETDATE()");
+    }
+
+    [HttpGet("GetUsers")]
+    // public IEnumerable<User> GetUsers()
+    public IEnumerable<User> GetUsers()
+    {
+        string sql = @"
+            SELECT [UserId],
+                [FirstName],
+                [LastName],
+                [Email],
+                [Gender],
+                [Active] 
+            FROM TutorialAppSchema.Users";
+        IEnumerable<User> users = _dapper.LoadData<User>(sql);
+        return users;
+        // return new string[] {"user1", "user2" };
+        // return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        // {
+        //     Date = DateTime.Now.AddDays(index),
+        //     TemperatureC = Random.Shared.Next(-20, 55),
+        //     Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        // })
+        // .ToArray();
+    }
+
+    [HttpGet("GetSingleUser/{userId}")]
+    // public IEnumerable<User> GetUsers()
+    public User GetSingleUser(int userId)
+    {
+        string sql = @"
+            SELECT [UserId],
+                [FirstName],
+                [LastName],
+                [Email],
+                [Gender],
+                [Active] 
+            FROM TutorialAppSchema.Users
+                WHERE UserId = " + userId.ToString(); //"7"
+        User user = _dapper.LoadDataSingle<User>(sql);
+        return user;
+    }
+}
